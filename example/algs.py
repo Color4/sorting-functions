01e73ea8e512477fc51a9fa8f96bea5864f8497b
Christa.Caggiano <Christa.Caggiano@UCSFs-MacBook-Pro.local>
2018-01-24 10:08:58 -0800

    added comments

--- example/algs.py
+++ example/algs.py
@@ -1,33 +1,60 @@
-import numpy as np
+# quick sort and bubble sort implementation hw
+# jan 2018
+# author <christa.caggiano@ucsf.edu>
+
 import random
+
 
 def bubble_sort(l):
     """
-    Describe how you are sorting `x`
+    :param l: unsorted list
+    :return: sorted list
+
+    keeping track of whether the list is sorted,
+    iterate over the list. If the ith index is less than
+    the ith + 1 index, swap them. Continue until you make
+    no swaps. Return list.
+
     """
-    not_sorted = True
+    not_sorted = True # start with list unsorted
     while not_sorted:
         not_sorted = False
         for i in range(len(l)-1):
             if l[i] > l[i+1]:
-                l[i+1], l[i] = l[i], l[i+1]
-                not_sorted = True
+                l[i+1], l[i] = l[i], l[i+1]  # pythonic swap !
+                not_sorted = True  # if a swap occurs, list is unsorted
     return l
 
 def quick_sort(l):
     """
-    Describe how you are sorting `x`
+    :param l: unsorted list
+    :return: sorted list
+
+    pick a random pivot. Put all the items in the list that
+    are less than the pivot in a 'less than list' or greater than in a
+    'greater than' list. Recursively call quick_sort on these two lists
+    so that a new pivot is chosen each time, sorting smaller and smaller lists
+    until the sorted list is returned.
     """
-    if len(l) == 0 or len(l) == 1:
+
+    if len(l) == 0 or len(l) == 1:  # base case, where list is empty or 1 item
         return l
-    pivot = random.randint(0, len(l)-1)
+
+    pivot = random.randint(0, len(l)-1)  # pick random pivot
+
+    # initialize lists for sorting
     less_than = []
     greater_than = []
 
+    # for each index in l, if the index isn't the pivot,
+    # add to appropriate list
     for index, item in enumerate(l):
         if not index == pivot:
             if item < l[pivot]:
                 less_than.append(item)
             else:
                 greater_than.append(item)
+
+    # recursively call the sorting function until a sorted list is returned
     return quick_sort(less_than) + [l[pivot]] + quick_sort(greater_than)
+
--- test/test_algs.py
+++ test/test_algs.py
@@ -1,29 +1,35 @@
-import numpy as np
+# quick sort and bubble sort implementation hw
+# jan 2018
+# author <christa.caggiano@ucsf.edu>
+
 from example import algs
 
 
+# test bubble sort
 class TestBubbleSort(object):
 
-    def test_empty(self):
+    def test_empty(self):  # test empty list
         x = []
         assert algs.bubble_sort(x) == []
 
-    def test_one_item(self):
+    def test_one_item(self):  # test one item list
         x = [1]
         assert algs.bubble_sort(x) == [1]
 
-    def test_many_items(self):
+    def test_many_items(self):  # list with many items and negative items
         x = [1, 2, 10, 0, 1, -1, 4, 8]
         assert algs.bubble_sort(x) == [-1, 0, 1, 1, 2, 4, 8, 10]
 
-    def test_in_order(self):
+    def test_in_order(self):  # test an ordered list
         x = [1, 2, 3, 4, 5]
         assert algs.bubble_sort(x) == [1, 2, 3, 4, 5]
 
-    def test_reverse_order(self):
+    def test_reverse_order(self):  # test a list completely out of order
         x = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         assert algs.bubble_sort(x) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
+# test quick sort  with same tests as above 
 class TestQuickSort(object):
 
     def test_empty(self):
